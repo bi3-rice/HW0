79bff295c77e79d08f5db59b58ba0318bb906f41
bi3-rice <bi3@rice.edu>
2023-09-10 19:08:20 -0500

    Test initial commit

---
+++ Another Python File.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy
+
+x = [[6, 3, 4],
+     [5, 6, 7],
+     [8, 9, 10]]
+print(np.linalg.det(x))
---
+++ main.py
@@ -0,0 +1,7 @@
+
+print("Hello World!\n")
+x = 4
+y = 3
+print(x * y)
+print(x / y)
+print(4 % 3)
